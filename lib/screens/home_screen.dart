b62ff157209ac4aba795c25c56a4113a2a926d35
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-02-07 09:13:49 +0500

    Adding Data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
 
+// UPDATING
   Future<void> _update(DocumentSnapshot? documentSnapshot) async {
     if (documentSnapshot != null) {
       // when thest fields are shown up they will be filled with previous data
@@ -85,9 +86,73 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+// ADDING
+  Future<void> _create() async {
+    // as we have same TextEditing controller for update and create so,
+    // we have to make empty firstly
+    _nameController.text = "";
+    _priceController.text = "";
+    // when user click update a bottom sheat will come, where user enter data
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+              // prevent the soft keyboard from converting text fields
+              bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+          child: Column(
+            // take as many height as it required
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                controller: _priceController,
+                decoration: const InputDecoration(labelText: 'Price'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final String name = _nameController.text;
+                  final double? price = double.tryParse(_priceController.text);
+
+                  if (price != null) {
+                    await _products.add({"name": name, "price": price});
+                    _nameController.text = "";
+                    _priceController.text = "";
+                  }
+                },
+                child: const Text("Add"),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floating action button for adding data
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _create();
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       // StreamBuilder help us to create a presistence connection with firestore database
       // As we update date because of streamBuilder we get updated data in real time immedatity
       body: StreamBuilder(
